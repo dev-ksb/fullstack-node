41e348cb7249805ff8e6f698734a4be1d86e6460
kuldeep singh <devkuldeeps89@gmail.com>
2022-10-07 23:03:40 +0530

    feat: google-Oauth integration with passportjs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const { googleClientID, googleClientSecret } = require("./config/keys");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// app.use(express.json());
 
-const PORT = process.env.PORT || 8000;
-app.listen(5000);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
